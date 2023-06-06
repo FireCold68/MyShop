17a345a93bae6ca01e417f02e46af5858e3ac47f
FireCold68 <118595414+FireCold68@users.noreply.github.com>
2023-06-06 13:12:36 +0300

    0606232

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+mongoose.connect(process.env.MONGO_URL)
+.then(results => {
+    //console.log(results)
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
 })
+.catch(error => {
+    console.log(error);
+})
+
